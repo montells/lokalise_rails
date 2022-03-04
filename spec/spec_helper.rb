25b0e7542039240d64fda8f89e10f8bdc602a5b5
Michel Sánchez Montells <montells@aleph.engineering>
2022-03-04 00:05:48 -0500

    Configure rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,28 @@
 require 'bundler/setup'
 require 'lokalise_rails_montells'
 
+require 'dotenv/load'
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_filter 'spec/'
+  add_filter '.github/'
+  add_filter 'lib/generators/templates/'
+  add_filter 'lib/lokalise_rails/version'
+end
+
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
+
+ENV['RAILS_ENV'] = 'test'
+
+require_relative '../spec/dummy/config/environment'
+ENV['RAILS_ROOT'] ||= "#{File.dirname(__FILE__)}../../../spec/dummy"
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
